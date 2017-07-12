687767ffcbd028f5291a4b38ad31d9ccf88a9282
Henry de la Hoz <cyberdelahoz95@gmail.com>
2017-07-11 22:07:42 -0500

    first commit

---
+++ test/client-test.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const test = require('ava')
+const platzigram = require('../')
+const fixtures = require('./fixtures')
+const nock = require('nock')
+
+let options = {
+  endpoints: {
+    pictures: 'https://platizgram.test/picture',
+    user: 'https://platizgram.test/user',
+    auth: 'https://platizgram.test/auth'
+  }
+}
+
+test.beforeEach(t => {
+  t.context.client = platzigram.createClient(options)
+})
+
+test('client', t => {
+  const client = t.context.client
+
+  t.is(typeof client.getPicture, 'function')
+  t.is(typeof client.savePicture, 'function')
+  t.is(typeof client.likePicture, 'function')
+  t.is(typeof client.listPictures, 'function')
+  t.is(typeof client.listPicturesByTag, 'function')
+  t.is(typeof client.saveUser, 'function')
+  t.is(typeof client.getUser, 'function')
+  t.is(typeof client.auth, 'function')
+})
+
+test('getPicture', async t => {
+  const client = t.context.client
+
+  let image = fixtures.getImage()
+
+  nock(options.endpoints.pictures)
+    .get(`/${image.publicId}`)
+    .reply(200, image)
+
+  let result = await client.getPicture(image.publicId)
+
+  t.deepEqual(image, result)
+})
